e3a9bba27cd6562634e604a8231c9acde879484b
Lars <larsvanderniet@gmail.com>
2020-06-05 00:05:25 +0200

    Made start of homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import logo from '../logo.svg';
-import '../assets/css/App.css';
 
+import '../assets/css/App.css';
+import '../assets/css/Home.css';
+
+import Index from '../assets/img/index.png';
 import Header from '../shared/Header';
+import Button from '../components/Button/index';
 
 
 const MainPage = () => {
-    return (
-      <Header />
-    );
+    return [
+      <Header />,
+      <section>
+        <img className="image" src={Index} alt="Foto van de binnentuin"/>
+      </section>,
+      <section className="content">
+        <h1 className="text text__welcome">Welkom bij de <span className="text text__secondary">Binnentuin</span></h1>
+        <h5 className="text text__sub">Hoe wil je bestellen?</h5>
+      </section>
+    ];
 }
 
 export default MainPage;
