1e7fe217ca9d14876585996663a07eac3dc3cbbc
drazero <ardkokkel@outlook.com>
2020-06-07 21:06:40 +0200

    Created options component to check which button is clicked

--- src/pages/keuze/Keuze.jsx
+++ src/pages/keuze/Keuze.jsx
@@ -2,33 +2,39 @@ import React from "react";
 import logo_white from '../../img/logo_white.png';
 import './Keuze.scss';
 
-const KeuzePage = () => {
-    return (
-      <div className="keuze">
+import OptieList from "../../pages/keuze/OptieList";
 
-        <header className="header">
-          <section className="container">
-            <figure className="header__figure">
-              <img src={logo_white} alt="Logo van Ruben en Jerry's eetcafé de Binnentuin"/>
-            </figure>
-          </section>
-        </header>
+class KeuzePage extends React.Component {
+    optieClicked = (id) => {
+      console.log(`Gekozen optie: ${id}`);
+    }
 
-        <section className="banner"></section> 
+    render() {
+      return(
+        <section className="keuze">
 
-        <article className="locatie">
-          <section className="locatie__heading">
-            <h1>Welkom bij <span className="locatie__heading--gold">de Binnentuin</span>!</h1>
-            <h2>Hoe wil je bestellen?</h2>
-          </section>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos molestias ad laboriosam facilis nostrum doloremque eos pariatur. Ad nesciunt dolorem quisquam assumenda fugit. Voluptatibus ab, minus natus odit quibusdam temporibus.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos molestias ad laboriosam facilis nostrum doloremque eos pariatur. Ad nesciunt dolorem quisquam assumenda fugit. Voluptatibus ab, minus natus odit quibusdam temporibus.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos molestias ad laboriosam facilis nostrum doloremque eos pariatur. Ad nesciunt dolorem quisquam assumenda fugit. Voluptatibus ab, minus natus odit quibusdam temporibus.</p>
-        </article>
-        
+          <header className="header">
+            <section className="container">
+              <figure className="header__figure">
+                <img src={logo_white} alt="Logo van Ruben en Jerry's eetcafé de Binnentuin"/>
+              </figure>
+            </section>
+          </header>
 
-      </div>
-    );
+          <section className="banner"></section> 
+
+          <article className="locatie">
+            <section className="locatie__heading">
+              <h1>Welkom bij <span className="locatie__heading--gold">de Binnentuin</span>!</h1>
+              <h2>Hoe wil je bestellen?</h2>
+            </section>
+          </article>
+
+          <OptieList optieClicked = {this.optieClicked} />
+
+        </section>
+      );
+    }
 }
 
 export default KeuzePage;
---
+++ src/pages/keuze/Optie.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo_white from '../../img/logo_white.png';
+import './Keuze.scss';
+
+class Optie extends React.Component {
+
+    onOptieClicked = () => {
+        this.props.optieClicked(this.props.id);
+    }
+
+    render(){
+        return (
+            <article>
+                <section className="optie" onClick = {this.onOptieClicked}>
+                    <p> {this.props.content || "debug"} </p>
+                    {this.props.title === "De Binnentuin" ? <img src={logo_white}/> : <h2>{this.props.title}</h2>}
+                </section>
+                <p>Geopend: 8.30 &mdash; 17.00</p>
+            </article>
+        );
+    }
+}
+
+export default Optie;
