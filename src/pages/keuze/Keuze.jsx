ae43b4e79d3b8af39f1ec9ed55677d2977d1c727
drazero <ardkokkel@outlook.com>
2020-06-10 17:01:44 +0200

    File Structure: Header and footer as component

--- src/pages/keuze/Keuze.jsx
+++ src/pages/keuze/Keuze.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import logo_white from '../../assets/img/logo_white.png';
 import './Keuze.scss';
 
+import Header from "../../components/header/Header"
 import Banner from "../../components/banner/Banner"
+import Footer from "../../components/footer/Footer"
 import OptieList from "../../pages/keuze/OptieList";
 
 class KeuzePage extends React.Component {
@@ -13,15 +14,7 @@ class KeuzePage extends React.Component {
     render() {
       return(
         <section className="keuze">
-
-          <header className="header">
-            <section className="container">
-              <figure className="header__figure">
-                <img src={logo_white} alt="Logo van Ruben en Jerry's eetcafé de Binnentuin"/>
-              </figure>
-            </section>
-          </header>
-
+          <Header />
           <Banner /> 
 
           <article className="locatie">
@@ -32,17 +25,7 @@ class KeuzePage extends React.Component {
           </article>
 
           <OptieList optieClicked = {this.optieClicked} />
-
-          <footer className="footer">
-            <section className="footer__adres">
-              <p>Langegracht 70, Leiden</p>
-              <p>2e verdieping PLNT en daktuin</p>
-            </section>
-            <section className="footer__social">
-              {/* Facebook, Twitter, Instagram */}
-            </section>
-          </footer>
-
+          <Footer />
         </section>
       );
     }
