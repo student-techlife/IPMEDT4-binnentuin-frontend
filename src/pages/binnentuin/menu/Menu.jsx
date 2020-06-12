b63a17a5fc7c9bcaaac02990e1d7df8e8da19b0e
Joeriubink <joeri.ubink@outlook.com>
2020-06-12 15:24:23 +0200

    added menu + salads placeholder list

--- src/pages/binnentuin/menu/Menu.jsx
+++ src/pages/binnentuin/menu/Menu.jsx
@@ -4,11 +4,19 @@ import './Menu.scss';
 
 import Header from "../../../components/header/Header"
 import CardList from "../../../pages/binnentuin/menu/CardList";
+import CardListSalads from "../../../pages/binnentuin/menu/CardListSalads";
 import Footer from "../../../components/footer/Footer"
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUtensils, faMortarPestle, faShoppingBasket} from "@fortawesome/free-solid-svg-icons";
 
 class MenuPage extends React.Component {
     cardClicked = (id) => {
-      console.log(`Gekozen gerecht nummer: + ${id}`);
+      console.log(`Gekozen gerecht nummer: ${id}`);
+    }
+
+    cardSaladClicked = (id) => {
+      console.log(`Gekozen gerecht nummer: ${id}`);
     }
 
     render() {
@@ -16,10 +24,30 @@ class MenuPage extends React.Component {
         <section>
 
           <Header />
+          <article className="menu">
+            <section className="menu_title">
+              <FontAwesomeIcon className="menu__list-icon" icon={faShoppingBasket}/><h2 className="menu_title--gold">Menu</h2>
+            </section>
+            <section className="menu__menu">
+              <a className="menu__menu__button active first" href="menu#snacks" type="button">Snacks</a>
+              <a className="menu__menu__button" href="menu#salads" type="button">Salads</a>
+              <a className="menu__menu__button" type="button">Toasts</a>
+              <a className="menu__menu__button last" type="button">Sandwiches</a>
+            </section>
+          </article>
 
+          <section id="snacks" className="menu_title">
+            <FontAwesomeIcon className="menu__list-icon" icon={faUtensils}/><h2 className="menu_title--gold">Snacks</h2>
+          </section>
           <CardList cardClicked = {this.cardClicked} />
 
+          <section id="salads" className="menu_title">
+            <FontAwesomeIcon className="menu__list-icon" icon={faMortarPestle}/><h2 className="menu_title--gold">Salads</h2>
+          </section>
+          <CardListSalads cardSaladClicked = {this.cardSaladClicked} />
+
           <Footer />
+
         </section>
       );
     }
