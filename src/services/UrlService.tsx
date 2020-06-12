2d4f00a6c4b96588bdf0562fcc94f22638e93a06
Klaas Schoute <klaas_schoute@hotmail.com>
2020-06-13 00:15:32 +0200

    :hammer: Set urlservice altijd op live server

--- src/services/UrlService.tsx
+++ src/services/UrlService.tsx
@@ -1,10 +1,10 @@
-let apiDomain = ''
+let apiDomain = 'https://admin.binnentuin.live/'
 
-if (process.env.NODE_ENV === 'development') {
-    apiDomain = 'https://admin.binnentuin.live/';
-} else {
-    apiDomain = 'http://127.0.0.1:8001/';
-}
+// if (process.env.NODE_ENV === 'development') {
+//     apiDomain = 'https://admin.binnentuin.live/';
+// } else {
+//     apiDomain = 'http://127.0.0.1:8001/';
+// }
 
 class UrlService {
     static loginUrl() { return apiDomain + 'api/login'; }
