cb1eac274bc1e7b1154357b7d6aec175c06089a4
Klaas Schoute <klaas_schoute@hotmail.com>
2020-06-25 13:23:27 +0200

    :hammer: Refactor code

--- src/pages/keuze/OptieList.js
+++ src/pages/keuze/OptieList.js
@@ -11,19 +11,20 @@ class OptieList extends React.Component {
     render(){
         return(
             <section className="opties opties--3 container">
-              <Link className="opties__link" to="../pretest/">
-                <Optie title="De Binnentuin"
+                <Link className="opties__link" to="/pretest">
+                    <Optie title="De Binnentuin"
                         content="Eetcafé"
                         id="Binnentuin"
                         optieClicked={this.optieClicked}
-                /></Link>
-              <Link className="opties__link" to="../pretest/">
-                <Optie title="The Roof"
+                    />
+                </Link>
+                <Link className="opties__link" to="/pretest">
+                    <Optie title="The Roof"
                         content="Daktuin"
                         id="The Roof"
                         optieClicked={this.optieClicked}
-                />
-            </Link>
+                    />
+                </Link>
             </section>
         );
     }
