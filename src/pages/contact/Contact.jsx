5b1dedcec6977d4be2f1ad9226efd075672f10d3
Anne Sophie Huibregtsen <s1114664@student.hsleiden.nl>
2020-06-30 00:14:13 +0200

    added submission of pretest

---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+import Header from "../../components/header/Header";
+import './Contact.scss';
+
+
+import {faCheck, faHandsWash, faPeopleArrows, faSoap} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Optie from "../Optie/Optie";
+
+
+class ContactPage extends React.Component {
+
+    render() {
+        return (
+            <section className="contact">
+                <Header/>
+                <article className="contact__content">
+                    <section className="contact__info">
+                        <h1 className="contact__title">
+                            <span className="contact__title">Openingstijden</span></h1>
+                    </section>
+                    <section>
+                        <h3 className="contact__subtitle">Binnentuin</h3>
+                        <table className="table">
+                            <tbody className="table__body">
+                            <tr>
+                                <th className="table__day">Maandag</th>
+                                <td className="table__time">8:30 - 17:00</td>
+                            </tr>
+                            <tr>
+                                <th className="table__day">Dinsdag</th>
+                                <td className="table__time">8:30 - 17:00</td>
+                            </tr>
+                            <tr>
+                                <th className="table__day">Woensdag</th>
+                                <td className="table__time">8:30 - 17:00</td>
+                            </tr>
+                            <tr>
+                                <th className="table__day">Donderdag</th>
+                                <td className="table__time">8:30 - 17:00</td>
+                            </tr>
+                            <tr>
+                                <th className="table__day">Vrijdag</th>
+                                <td className="table__time">8:30 - 17:00</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </section>
+                    <section>
+                        <h3 className="contact__subtitle">The Roof</h3>
+                        <table className="table">
+                            <tbody className="table__body">
+                            <tr>
+                                <th className="table__day">Maandag</th>
+                                <td className="table__time">8:30 - 17:00</td>
+                            </tr>
+                            <tr>
+                                <th className="table__day">Dinsdag</th>
+                                <td className="table__time">8:30 - 17:00</td>
+                            </tr>
+                            <tr>
+                                <th className="table__day">Woensdag</th>
+                                <td className="table__time">8:30 - 17:00</td>
+                            </tr>
+                            <tr>
+                                <th className="table__day">Donderdag</th>
+                                <td className="table__time">8:30 - 17:00</td>
+                            </tr>
+                            <tr>
+                                <th className="table__day">Vrijdag</th>
+                                <td className="table__time">8:30 - 17:00</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </section>
+                </article>
+            </section>
+        );
+    }
+}
+
+export default ContactPage;
--- src/pages/pre-test/Pretest.jsx
+++ src/pages/pre-test/Pretest.jsx
@@ -28,11 +28,6 @@ class PretestPage extends React.Component {
         this.onAfhalenClicked = this.onAfhalenClicked.bind(this);
     }
 
-    handleSubmit(event) {
-        alert('De tijd van jou reservering is ' + this.state.activeTijd);
-        event.preventDefault();
-    }
-
     onBinnentuinClicked(event) {
         this.setState({
             pretest: true,
@@ -53,6 +48,26 @@ class PretestPage extends React.Component {
         this.setState({activeTijd: event.target.value});
     }
 
+    handleSubmit = event => {
+        event.preventDefault();
+        const data = new FormData(event.target);
+
+        // Todo: data.set(..., ...);
+        for (var pair of data.entries()) {
+            console.log(pair);
+        }
+
+        fetch('http://192.168.1.112:8000/api/pretest', {
+            method: 'POST',
+            body: data,
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Todo: error handeling als er een error is.
+                console.log(data);
+            });
+    }
+
     render() {
         return (
             <section className="pretest">
@@ -63,88 +78,94 @@ class PretestPage extends React.Component {
                     </section>
                     <section className=" opties">
                         <Optie title="De Binnentuin"
-                            content="Eetcafé"
-                            id="Binnentuin"
-                            optieClicked={this.onBinnentuinClicked}
+                               content="Eetcafé"
+                               id="Binnentuin"
+                               optieClicked={this.onBinnentuinClicked}
                         />
                         <Optie title="Afhalen"
-                            content=" "
-                            id="Afhalen"
-                            optieClicked={this.onAfhalenClicked}
+                               content=" "
+                               id="Afhalen"
+                               optieClicked={this.onAfhalenClicked}
                         />
                     </section>
                 </article>
 
                 <article className="pretest__content">
-                    <section style={{display: this.state.pretest ? 'block' : 'none'}} className="pretest__info">
-                        <h1 className="prestest__title">
-                            <span className="pretest__title pretest__title--normal">Pré controle</span>
-                            <span className="pretest__title pretest__title--bold">Covid-19</span></h1>
-                        <p className="pretest__subtitle">Als binnentuin zorgen wij ervoor dat: </p>
-                        <ul className="pretest__list">
-                            <li className="pretest__listitem">
-                                <FontAwesomeIcon className="pretest__list-icon" icon={faHandsWash}/>
-                                <p className="pretest__text">Wassen en ontsmetten onze handen na elke bestelling</p>
-                            </li>
-                            <li className="pretest__listitem">
-                                <FontAwesomeIcon className="pretest__list-icon" icon={faPeopleArrows}/>
-                                <p className="pretest__text">Waarborgen de 1,5 meter afstand</p>
-                            </li>
-                            <li className="pretest__listitem">
-                                <FontAwesomeIcon className="pretest__list-icon" icon={faPeopleArrows}/>
-                                <p className="pretest__text">Serveren en ruimen af op minimaal 1,5 meter afstand</p>
-                            </li>
-                            <li className="pretest__listitem">
-                                <FontAwesomeIcon className="pretest__list-icon" icon={faSoap}/>
-                                <p className="pretest__text">Zorgen voor een grondige reinging van o.a. tafels, stoelen en menukaarten</p>
-                            </li>
-                        </ul>
-                    </section>
-                    <section style={{display: this.state.pretest ? 'block' : 'none'}} className="pretest__checklist">
-                        <h2 className="pretest__secundarytitle">Checklist</h2>
-                        <p className="pretest__text">Met hoeveel personen wilt u komen?</p>
-                        <input className="pretest__amount" type="numeric" placeholder="1"/>
-                        <p className="pretest__text">Heeft u of een huisgenoot last gehad van hoesten, neusverkoudheid,
-                            benauwdheidsklachten of koorts vanaf 38 graden?</p>
-                        <fieldset className="pretest__checkboxes">
-                            <div className="pretest__checkbox-field">
-                                <input id="ja" className="pretest__checkbox" name="choice" type="radio"/>
-                                <label className="pretest__checkbox-box" htmlFor="ja"><FontAwesomeIcon icon={faCheck}/></label>
-                                <label className="pretest__checkbox-label" htmlFor="ja">Ja</label>
-                            </div>
-                            <div className="pretest__checkbox-field">
-                                <input checked id="nee" className="pretest__checkbox" name="choice" type="radio"/>
-                                <label className="pretest__checkbox-box" htmlFor="nee"><FontAwesomeIcon icon={faCheck}/></label>
-                                <label className="pretest__checkbox-label" htmlFor="nee">Nee</label>
-                            </div>
-                        </fieldset>
-                    </section>
-                    <section className="pretest__reserveren"
-                            style={{display: this.state.time ? 'block' : 'none'}}>
-                        <h2 className="pretest__secundarytitle__reserveren">&#8635;<span
-                            className="pretest__secundarytitle__reserveren--gold">  Tijd reserveren</span></h2>
-                        <p className="pretest__text__reserveren">U kunt een plek in het restaurant reserveren. Dit houdt
-                            in dat er een
-                            plek vrijgehouden wordt gedurende het gekozen tijdstip. Op locatie kunt u uw stoel/ tafel
-                            kiezen.</p>
-                        <label>
-                            <select className="pretest__dropdown" value={this.state.activeTijd}
-                                    onChange={this.handleChange}>
-                                {this.state.tijden.map(tijd => (
-                                    <option key={tijd.id} value={tijd.tijd}>
-                                        {tijd.tijd}
-                                    </option>
-                                ))}
-                            </select>
-                        </label>
-                    </section>
-                    <section style={{display: this.state.time ? 'block' : 'none'}}>
-                        <form className="pretest__button" onSubmit={this.handleSubmit}>
-                            <Link to="/binnentuin/menu">
-                                <input className="pretest__button__input" type="submit" value="Reservering bevestigen"/>
-                            </Link>
-                        </form>
-                    </section>
+                    <form onSubmit={this.handleSubmit}>
+                        <section style={{display: this.state.pretest ? 'block' : 'none'}} className="pretest__info">
+                            <h1 className="prestest__title">
+                                <span className="pretest__title pretest__title--normal">Pré controle</span>
+                                <span className="pretest__title pretest__title--bold">Covid-19</span></h1>
+                            <p className="pretest__subtitle">Als binnentuin zorgen wij ervoor dat: </p>
+                            <ul className="pretest__list">
+                                <li className="pretest__listitem">
+                                    <FontAwesomeIcon className="pretest__list-icon" icon={faHandsWash}/>
+                                    <p className="pretest__text">Wassen en ontsmetten onze handen na elke bestelling</p>
+                                </li>
+                                <li className="pretest__listitem">
+                                    <FontAwesomeIcon className="pretest__list-icon" icon={faPeopleArrows}/>
+                                    <p className="pretest__text">Waarborgen de 1,5 meter afstand</p>
+                                </li>
+                                <li className="pretest__listitem">
+                                    <FontAwesomeIcon className="pretest__list-icon" icon={faPeopleArrows}/>
+                                    <p className="pretest__text">Serveren en ruimen af op minimaal 1,5 meter afstand</p>
+                                </li>
+                                <li className="pretest__listitem">
+                                    <FontAwesomeIcon className="pretest__list-icon" icon={faSoap}/>
+                                    <p className="pretest__text">Zorgen voor een grondige reinging van o.a. tafels,
+                                        stoelen en menukaarten</p>
+                                </li>
+                            </ul>
+                        </section>
+                        <section style={{display: this.state.pretest ? 'block' : 'none'}}
+                                 className="pretest__checklist">
+                            <h2 className="pretest__secundarytitle">Checklist</h2>
+                            <p className="pretest__text">Met hoeveel personen wilt u komen?</p>
+                            <input className="pretest__amount" type="numeric" placeholder="1" value="1" name="persons"
+                                   id="persons"/>
+                            <p className="pretest__text">Heeft u of een huisgenoot last gehad van hoesten,
+                                neusverkoudheid,
+                                benauwdheidsklachten of koorts vanaf 38 graden?</p>
+                            <fieldset className="pretest__checkboxes">
+                                <div className="pretest__checkbox-field">
+                                    <input id="ja" className="pretest__checkbox" name="symptoms" type="radio"
+                                           value="1"/>
+                                    <label className="pretest__checkbox-box" htmlFor="ja"><FontAwesomeIcon
+                                        icon={faCheck}/></label>
+                                    <label className="pretest__checkbox-label" htmlFor="ja">Ja</label>
+                                </div>
+                                <div className="pretest__checkbox-field">
+                                    <input checked id="nee" className="pretest__checkbox" name="symptoms" type="radio"
+                                           value="0"/>
+                                    <label className="pretest__checkbox-box" htmlFor="nee"><FontAwesomeIcon
+                                        icon={faCheck}/></label>
+                                    <label className="pretest__checkbox-label" htmlFor="nee">Nee</label>
+                                </div>
+                            </fieldset>
+                        </section>
+                        <section className="pretest__reserveren"
+                                 style={{display: this.state.time ? 'block' : 'none'}}>
+                            <h2 className="pretest__secundarytitle__reserveren">&#8635;<span
+                                className="pretest__secundarytitle__reserveren--gold">  Tijd reserveren</span></h2>
+                            <p className="pretest__text__reserveren">U kunt een plek in het restaurant reserveren. Dit
+                                houdt
+                                in dat er een
+                                plek vrijgehouden wordt gedurende het gekozen tijdstip. Op locatie kunt u uw stoel/
+                                tafel
+                                kiezen.</p>
+                            <label>
+                                <select className="pretest__dropdown" value={this.state.activeTijd}
+                                        onChange={this.handleChange} name="reservation_time" id="reservation_time">
+                                    {this.state.tijden.map(tijd => (
+                                        <option key={tijd.id} value={tijd.tijd}>
+                                            {tijd.tijd}
+                                        </option>
+                                    ))}
+                                </select>
+                            </label>
+                        </section>
+                        <input type='submit' style={{display: this.state.time ? 'block' : 'none'}}/>
+                    </form>
                 </article>
             </section>
         );
