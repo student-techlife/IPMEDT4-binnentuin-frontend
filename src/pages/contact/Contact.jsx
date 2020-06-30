a988f7e93ada02af41f6196840f1df0cdb8f1af8
drazero <ardkokkel@outlook.com>
2020-06-30 21:11:14 +0200

    Vraag openingstijden op vanuit API voor contact pagina

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,10 +1,111 @@
 import React from "react";
+import UrlService from "../../services/UrlService";
 
 import Header from "../../components/header/Header";
 import './Contact.scss';
-
+import axios from "axios";
 
 class ContactPage extends React.Component {
+
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            Openingstijden: []
+        };
+    }
+
+    getOpeningstijden() {
+        // Vraag de dag op en vertaal die naar een Nederlandse string
+        const dag = new Date().toLocaleString('nl-nl', {  weekday: 'long' });
+
+        // Zet variable nummer naar de index van de huidige dag om in api op te vragen
+        let nummer = 0;
+        switch(dag) {
+            case 'maandag':
+                nummer = 0;
+                break;
+            case 'dinsdag':
+                nummer = 1;
+                break;
+            case 'woensdag':
+                nummer = 2;
+                break;
+            case 'donderdag':
+                nummer = 3;
+                break;
+            case 'vrijdag':
+                nummer = 4;
+                break;
+            case 'zaterdag':
+                nummer = 5;
+                break;
+            case 'zondag':
+                nummer = 6;
+                break;
+            default:
+                nummer = 0;
+                break;
+        }
+
+        // De Binnentuin
+        axios.get(UrlService.BinnentuinOpeningstijden(), {}).then(res => {
+            const data = res.data
+            
+            // States
+            let statusMaandag = data[0].status === 1 ? data[0].openingstijd + ' - ' + data[0].sluitingstijd : 'Gesloten';
+            let statusDinsdag = data[1].status === 1 ? data[1].openingstijd + ' - ' + data[1].sluitingstijd : 'Gesloten';
+            let statusWoensdag = data[2].status === 1 ? data[2].openingstijd + ' - ' + data[2].sluitingstijd : 'Gesloten';
+            let statusDonderdag = data[3].status === 1 ? data[3].openingstijd + ' - ' + data[3].sluitingstijd : 'Gesloten';
+            let statusVrijdag = data[4].status === 1 ? data[4].openingstijd + ' - ' + data[4].sluitingstijd : 'Gesloten';
+            let statusZaterdag = data[5].status === 1 ? data[5].openingstijd + ' - ' + data[5].sluitingstijd : 'Gesloten';
+            let statusZondag = data[6].status === 1 ? data[6].openingstijd + ' - ' + data[6].sluitingstijd : 'Gesloten';
+
+            this.setState({
+                statusMaandag,
+                statusDinsdag,
+                statusWoensdag,
+                statusDonderdag,
+                statusVrijdag,
+                statusZaterdag,
+                statusZondag,
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
+        // The Roof
+        axios.get(UrlService.TheRoofOpeningstijden(), {}).then(res => {
+            const dataTheRoof = res.data
+
+            // States
+            let statusRoofMaandag = dataTheRoof[0].status === 1 ? dataTheRoof[0].openingstijd + ' - ' + dataTheRoof[0].sluitingstijd : 'Gesloten';
+            let statusRoofDinsdag = dataTheRoof[1].status === 1 ? dataTheRoof[1].openingstijd + ' - ' + dataTheRoof[1].sluitingstijd : 'Gesloten';
+            let statusRoofWoensdag = dataTheRoof[2].status === 1 ? dataTheRoof[2].openingstijd + ' - ' + dataTheRoof[2].sluitingstijd : 'Gesloten';
+            let statusRoofDonderdag = dataTheRoof[3].status === 1 ? dataTheRoof[3].openingstijd + ' - ' + dataTheRoof[3].sluitingstijd : 'Gesloten';
+            let statusRoofVrijdag = dataTheRoof[4].status === 1 ? dataTheRoof[4].openingstijd + ' - ' + dataTheRoof[4].sluitingstijd : 'Gesloten';
+            let statusRoofZaterdag = dataTheRoof[5].status === 1 ? dataTheRoof[5].openingstijd + ' - ' + dataTheRoof[5].sluitingstijd : 'Gesloten';
+            let statusRoofZondag = dataTheRoof[6].status === 1 ? dataTheRoof[6].openingstijd + ' - ' + dataTheRoof[6].sluitingstijd : 'Gesloten';
+
+            this.setState({
+                statusRoofMaandag,
+                statusRoofDinsdag,
+                statusRoofWoensdag,
+                statusRoofDonderdag,
+                statusRoofVrijdag,
+                statusRoofZaterdag,
+                statusRoofZondag,
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    componentDidMount() {
+        this.getOpeningstijden();
+    }
 
     render() {
         return (
@@ -21,23 +122,31 @@ class ContactPage extends React.Component {
                             <tbody className="table__body">
                             <tr>
                                 <th className="table__day">Maandag</th>
-                                <td className="table__time">8:30 - 17:00</td>
+                                <td className="table__time">{this.state.statusMaandag}</td>
                             </tr>
                             <tr>
                                 <th className="table__day">Dinsdag</th>
-                                <td className="table__time">8:30 - 17:00</td>
+                                <td className="table__time">{this.state.statusDinsdag}</td>
                             </tr>
                             <tr>
                                 <th className="table__day">Woensdag</th>
-                                <td className="table__time">8:30 - 17:00</td>
+                                <td className="table__time">{this.state.statusWoensdag}</td>
                             </tr>
                             <tr>
                                 <th className="table__day">Donderdag</th>
-                                <td className="table__time">8:30 - 17:00</td>
+                                <td className="table__time">{this.state.statusDonderdag}</td>
                             </tr>
                             <tr>
                                 <th className="table__day">Vrijdag</th>
-                                <td className="table__time">8:30 - 17:00</td>
+                                <td className="table__time">{this.state.statusVrijdag}</td>
+                            </tr>
+                            <tr>
+                                <th className="table__day">Zaterdag</th>
+                                <td className="table__time">{this.state.statusZaterdag}</td>
+                            </tr>
+                            <tr>
+                                <th className="table__day">Zondag</th>
+                                <td className="table__time">{this.state.statusZondag}</td>
                             </tr>
                             </tbody>
                         </table>
@@ -48,23 +157,31 @@ class ContactPage extends React.Component {
                             <tbody className="table__body">
                             <tr>
                                 <th className="table__day">Maandag</th>
-                                <td className="table__time">8:30 - 17:00</td>
+                                <td className="table__time">{this.state.statusRoofMaandag}</td>
                             </tr>
                             <tr>
                                 <th className="table__day">Dinsdag</th>
-                                <td className="table__time">8:30 - 17:00</td>
+                                <td className="table__time">{this.state.statusRoofDinsdag}</td>
                             </tr>
                             <tr>
                                 <th className="table__day">Woensdag</th>
-                                <td className="table__time">8:30 - 17:00</td>
+                                <td className="table__time">{this.state.statusRoofWoensdag}</td>
                             </tr>
                             <tr>
                                 <th className="table__day">Donderdag</th>
-                                <td className="table__time">8:30 - 17:00</td>
+                                <td className="table__time">{this.state.statusRoofDonderdag}</td>
                             </tr>
                             <tr>
                                 <th className="table__day">Vrijdag</th>
-                                <td className="table__time">8:30 - 17:00</td>
+                                <td className="table__time">{this.state.statusRoofVrijdag}</td>
+                            </tr>
+                            <tr>
+                                <th className="table__day">Zaterdag</th>
+                                <td className="table__time">{this.state.statusRoofZaterdag}</td>
+                            </tr>
+                            <tr>
+                                <th className="table__day">Zondag</th>
+                                <td className="table__time">{this.state.statusRoofZondag}</td>
                             </tr>
                             </tbody>
                         </table>
